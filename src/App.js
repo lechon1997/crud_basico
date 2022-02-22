fab78adca6c978860cc9e48565c2fc45c8197a52
Leonardo <odriozola-leo@hotmail.com>
2022-02-22 15:51:40 -0300

    listar usuario y agregar usuario function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import UserTable from "./components/UserTable";
+import { useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+	const usersData = [
+		{ id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+		{ id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+		{ id: uuidv4(), name: 'Ben', username: 'benisphere' },
+	  ]
+	
+	const [users, setUsers] = useState(usersData)
+	const addUser = (user) => {
+		user.id = users.length + 1
+		setUsers([...users, user])
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="container">
+	<h1>CRUD App with Hooks</h1>
+	<div className="flex-row">
+	  <div className="flex-large">
+		<h2>Add user</h2>
+		<AddUserForm addUser={addUser} />
+	  </div>
+	  <div className="flex-large">
+		<h2>View users</h2>
+		<UserTable users={users} />
+	  </div>
+	</div>
+  </div>
   );
 }
 
